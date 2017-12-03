f624e49941c35117664e77b5a02b1c63cdb38812
Alex Gir <ivanovaa7863@mail.ru>
2017-12-03 20:44:17 +0300

    Add context

---
+++ Sport.Data/Context.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sport.Data
+{
+    class Context: DbContext
+    {
+        public DbSet<Manufacturer> Manufacturers { get; set; }
+        public DbSet<Model> Models { get; set; }
+        public DbSet<Car> Cars { get; set; }
+
+        public Context() : base("localsql")
+        {
+
+        }
+    }
+}
